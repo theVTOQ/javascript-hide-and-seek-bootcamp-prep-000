ec2ceca0260053261cbc697a82b265e1ffb162ee
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 22:45:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target');
 }
+
+function deepestChild(){
+  var grandChildren = document.querySelectorAll('#grand-node ' div);
+  return grandChildren[grandChildren.length - 1]
+}
+
+function increaseRankBy(n){
+  
+}
