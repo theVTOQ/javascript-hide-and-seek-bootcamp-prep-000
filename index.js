e2e2136307e58d0d3013b67ff5805e1c11d69885
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 22:51:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  var grandChildren = document.querySelectorAll('#grand-node div');
+  return grandChildren[grandChildren.length - 1]
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('.ranked-list li');
+
+  for(var i = 0; i < list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
